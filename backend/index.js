1b70b4ef5e6d64f51fb81ab435a188c5202fe11b
Bipul <mhbipul99bd@gmail.com>
2025-08-14 01:34:06 +0600

    defined user model + some internal setup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 import express from "express"
 import { connectDB } from "./lib/db.js";
+import userRoutes from "./routes/user.route.js"; // Import user routes
+import dotenv from "dotenv";
+dotenv.config(); 
 
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json()); // Middleware to parse JSON bodies 
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+// Import routes
+app.use("/api/users",userRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    // Connect to the database when the server starts   
     connectDB();
 });
