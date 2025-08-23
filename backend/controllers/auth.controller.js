c0fd6a2fe84a814a98d2037743c6df40b4350732
Bipul <mhbipul99bd@gmail.com>
2025-08-23 22:27:47 +0600

    signIn api+route created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from '../lib/error.js';
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const signup =async (req, res, next) => { 
   
@@ -23,5 +24,39 @@ export const signup =async (req, res, next) => {
     } catch (error) {
         next(error)
         
+    }
+}
+
+export const singin = async (req, res, next) => {  
+    try{
+        const {email, password} = req.body;
+        //find user by email
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, "User not found"));
+
+        //compare password
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(400, "Wrong Credentials"));
+
+        //creating token
+        const token =  jwt.sign({id : validUser._id},process.env.JWT_SECRET, {expiresIn: "1d"});
+
+        //removing password from user object before sending response
+        const {password: pwd, ...rest} = validUser._doc;
+
+        //cookie 
+        res.cookie("access_token", token, {
+            httpOnly: true,
+            expires : new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+        }).status(200).json(rest);// sending user data without password
+
+
+
+
+    } 
+    catch(error) {
+        next(error)
     }
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signup, singin } from '../controllers/auth.controller.js';
 
 
 const router = express.Router();
 
 router.post('/signup',signup)
+router.post('/signin', singin); // Assuming you want to use the same controller for signin
 
 export default router;
